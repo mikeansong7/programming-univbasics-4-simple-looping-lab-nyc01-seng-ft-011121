b8551b3da45d3bc9336eeb1c8040ca0e48bf4f21
Michael B Ansong <michael.ansong7@gmail.com>
2020-12-28 16:12:01 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,7 +1,7 @@
 # Write your methods here
 def loop_message_five_times(message)
   counter = 0 
-  while message [counter] <5 do
+  while message [counter]<5 do
     puts message [counter]
     counter += 1 
   end
@@ -22,6 +22,7 @@ def output_array(array)
     counter += 1
   end
 end
+
 
 
   
