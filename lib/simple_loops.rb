eaac74cee489307c61b907f545884f92ce7f6231
Michael B Ansong <michael.ansong7@gmail.com>
2020-12-28 16:16:36 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -7,9 +7,9 @@ def loop_message_five_times(message)
   end
 end
 
-def loop_message_n_times(message, number)
+def loop_message_n_times(message, limit)
 number = 0
-  while number<10 do
+  while number < limit do
     puts message 
     number += 1 
   end
